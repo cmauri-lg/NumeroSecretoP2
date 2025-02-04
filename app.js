e10916968e4285aa1a698b180fb6cc6c33ce8085
cmauri-lg <cmauri.lg@gmail.com>
2025-02-04 12:57:05 -0600

    Funciones - Asignar texto a un elemento HTML

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Juego del numero secreto';
-
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'indica un numero del 1 al 10';
-
 function intentoDeUsuario(){
     alert('Click desde el boton');
 }
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
+
+asignarTextoElemento('h1','Juego del numero secreto!');
+asignarTextoElemento('p','Indica un numero del 1 al 10');
