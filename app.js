ddc3dd9c2646c10cc8e8cb76f2db8ae31aa17dea
cmauri-lg <cmauri.lg@gmail.com>
2025-02-04 13:26:00 -0600

    Genera un número aleatorio

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+//Variable de ambito global
+let numeroSecreto = generarNumeroSecreto();
+
+console.log(numeroSecreto);
+
 function intentoDeUsuario(){
     alert('Click desde el boton');
+    return;//No retorna nada pero se coloca como buena practica
 }
 
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
+}
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1;
 }
 
 asignarTextoElemento('h1','Juego del numero secreto!');
