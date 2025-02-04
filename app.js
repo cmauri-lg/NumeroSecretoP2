83063db6b8ebf324257e4be4107ff9380a02efa0
cmauri-lg <cmauri.lg@gmail.com>
2025-02-04 15:11:08 -0600

    Capturando el valor del input

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 //Variable de ambito global
 let numeroSecreto = generarNumeroSecreto();
 
-console.log(numeroSecreto);
-
-function intentoDeUsuario(){
-    alert('Click desde el boton');
-    return;//No retorna nada pero se coloca como buena practica
-}
-
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
     return;
+}
+
+function verificarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);//Por tener solo un input, si no usar funcion.
+    console.log(typeof(numeroDeUsuario));
+    console.log(typeof(numeroSecreto));
+    console.log(numeroDeUsuario);
+    console.log(numeroSecreto);
+    console.log(numeroDeUsuario === numeroSecreto);//Triple comparacion para ver si es mismo valor mismo tipo de dato
+    return;//No retorna nada pero se coloca como buena practica
 }
 
 function generarNumeroSecreto() {
