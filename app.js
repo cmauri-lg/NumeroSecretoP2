e3a5de2e7c5562fadf2f313ed0ee3254f627a8ef
cmauri-lg <cmauri.lg@gmail.com>
2025-02-07 09:37:38 -0600

    Mensaje de número intentos

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 //Variable de ambito global
 let numeroSecreto = generarNumeroSecreto();
-
+let intentos = 1; //Contador
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
     return;
 }
-
+console.log(numeroSecreto);
 function verificarIntento(){
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);//Por tener solo un input, si no usar funcion.
     
+    console.log(intentos);
     //Triple comparacion para ver si es mismo valor mismo tipo de dato
     if(numeroDeUsuario === numeroSecreto){
-        asignarTextoElemento('p','Acertaste el numero');
+        asignarTextoElemento('p',`Acertaste el numero en ${intentos} ${intentos === 1 ? 'vez' : 'veces'}`);
     }else{
         if(numeroDeUsuario>numeroSecreto){
             asignarTextoElemento('p','El numero es menor');
         }else{
             asignarTextoElemento('p','El numero es mayor');
         }
+        intentos++;//Incremento
     }
     return;//No retorna nada pero se coloca como buena practica
 }
